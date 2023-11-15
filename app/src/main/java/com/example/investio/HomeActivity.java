187023a93715861dd9e09bee68d80476f1346006
Hafiz Muhammad Ammar <ammarnadeem379@gmail.com>
2023-11-15 23:56:02 +0500

    MAKING OF STOCKS AND CRYPTO FRAGMENTS

--- app/src/main/java/com/example/investio/HomeActivity.java
+++ app/src/main/java/com/example/investio/HomeActivity.java
@@ -1,24 +1,57 @@
 package com.example.investio;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
+    Button btnfragstocks, btnfragcrypto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+        btnfragstocks = findViewById(R.id.btnfragstocks);
+        btnfragcrypto = findViewById(R.id.btnfragcrypto);
+
+        btnfragstocks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fm = getSupportFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                ft.add(R.id.container, new StocksFragment());
+                ft.commit();
+            }
+        });
+
+        btnfragcrypto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fm = getSupportFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                ft.add(R.id.container, new CryptoFragment());
+                ft.commit();
+            }
+        });
+
+
     }
 
-    public void logout(View view){
+
+    public void logout(View view) {
         FirebaseAuth.getInstance().signOut();
-        startActivity(new Intent(getApplicationContext(),LoginPage.class));
+        startActivity(new Intent(getApplicationContext(), LoginPage.class));
         finish();
+
     }
 }
