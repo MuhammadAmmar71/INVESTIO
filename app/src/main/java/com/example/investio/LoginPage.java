5c557dfa90c2d2205b255bcbb58d889e817ec2e7
Hafiz Muhammad Ammar <ammarnadeem379@gmail.com>
2023-11-15 13:21:52 +0500

    Authentication using firebase

--- app/src/main/java/com/example/investio/LoginPage.java
+++ app/src/main/java/com/example/investio/LoginPage.java
@@ -1,40 +1,95 @@
 package com.example.investio;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.investio.HomeActivity;
+import com.example.investio.SignupPage;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginPage extends AppCompatActivity {
-    Button btnSignin;
-    Button btnLoginSuccessful;
+    FirebaseAuth fAuth;
+    EditText edtlemail,edtlpswd;
+    Button btnlogin,btnnotreg;
+    Button btngoogle;
 
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
-        btnSignin=findViewById(R.id.btnSignin);
-        btnLoginSuccessful=findViewById(R.id.btnLoginSuccessful);
 
-        Intent iSignup = new Intent(LoginPage.this,SignupPage.class);
-        Intent iHome = new Intent(LoginPage.this,HomeActivity.class);
-        btnSignin.setOnClickListener(new View.OnClickListener() {
+        edtlemail=findViewById(R.id.edtlemail);
+        edtlpswd=findViewById(R.id.edtlpswd);
+        btnlogin=findViewById(R.id.btnlogin);
+        btnnotreg=findViewById(R.id.btnnotreg);
+
+        btngoogle=findViewById(R.id.btngoogle);
+
+        fAuth=FirebaseAuth.getInstance();
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                startActivity(iSignup);
+                String email = edtlemail.getText().toString().trim();
+                String password = edtlpswd.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+
+                    edtlemail.setError("Email is requires.");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+
+                    edtlpswd.setError("Password is required.");
+                    return;
+                }
+
+                if (password.length() < 6) {
+                    edtlpswd.setError("Password must be atleast 6 characters long.");
+                    return;
+                }
+
+                fAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(LoginPage.this,"Logged in Successful",Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                        }
+                        else{
+                            Toast.makeText(LoginPage.this,"Error !" + task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
             }
         });
 
-        btnLoginSuccessful.setOnClickListener(new View.OnClickListener() {
+        btnnotreg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), SignupPage.class));
+            }
+        });
 
-                startActivity(iHome);
+
+        btngoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginPage.this,GoogleSignin.class));
+
             }
         });
 
--- app/src/main/java/com/example/investio/SignupPage.java
+++ app/src/main/java/com/example/investio/SignupPage.java
@@ -1,27 +1,98 @@
 package com.example.investio;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignupPage extends AppCompatActivity {
+    EditText edtfname, edtemail, edtpswd, edtconfirmpswd;
+    Button btnalreg, btnreg;
+    FirebaseAuth fAuth;
 
-    Button btnSigninSuccessful;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_page);
-        Intent iLogin = new Intent(SignupPage.this, LoginPage.class);
-        btnSigninSuccessful=findViewById(R.id.btnSigninSuccessful);
-        btnSigninSuccessful.setOnClickListener(new View.OnClickListener() {
+
+        edtfname = findViewById(R.id.edtfname);
+        edtpswd = findViewById(R.id.edtpswd);
+        edtconfirmpswd = findViewById(R.id.edtconfirmpswd);
+        edtemail = findViewById(R.id.edtemail);
+        btnalreg = findViewById(R.id.btnalreg);
+        btnreg = findViewById(R.id.btnreg);
+
+        fAuth = FirebaseAuth.getInstance();
+
+        if (fAuth.getCurrentUser() != null) {
+            startActivity(new Intent(getApplicationContext(), LoginPage.class));
+            finish();
+        }
+
+
+        btnreg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(iLogin);
+
+                String email = edtemail.getText().toString().trim();
+                String password = edtpswd.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+
+                    edtemail.setError("Email is requires.");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+
+                    edtpswd.setError("Password is required.");
+                    return;
+                }
+
+                if (password.length() < 6) {
+                    edtpswd.setError("Password must be atleast 6 characters long.");
+                    return;
+                }
+
+                fAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()) {
+                            Toast.makeText(SignupPage.this,"User Created",Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+                        }
+                        else {
+                            Toast.makeText(SignupPage.this,"Error !" + task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+
+
             }
         });
+
+        btnalreg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ilogin = new Intent(getApplicationContext(), LoginPage.class);
+                startActivity(ilogin);
+            }
+        });
+
 
     }
 }
